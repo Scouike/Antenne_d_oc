c6b29daaa773880db84ea58d4547dca4f2ee7b62
AlexAmsif <47186124+AlexAmsif@users.noreply.github.com>
2020-03-09 00:00:44 +0100

    Update Emission.php

--- pages/Emission.php
+++ pages/Emission.php
@@ -33,25 +33,62 @@
 				/* inclu une barre de navigation */
 				include('bareNav/barreNav.html'); 
 			}
+			//connexion à la bd
+			$host = 'localhost';
+			$db   = 'bdradio';
+			$user = 'root';
+			$pass = 'root';
+			$charset = 'utf8mb4';
+			$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+			$options = [
+				PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES   => false,
+			];
+			try {	
+				 $pdo = new PDO($dsn, $user, $pass, $options);
+			} catch (PDOException $e) {
+				 throw new PDOException($e->getMessage(), (int)$e->getCode());
+			}
+			if($_GET["titre"]!=""){
+				Echo '<h1 class="text-uppercase m-4 text-center"> Emission : '.$_GET["titre"].' </h1>';
+				$sql="SELECT *
+					FROM Theme AS T 
+					JOIN liason AS L 
+					ON T.id_theme = L.id_theme
+					JOIN emission AS E
+					ON L.id_emission = E.id_emission
+					AND titre = ?";
+				$stmt = $pdo->prepare($sql);
+				$stmt->execute(array($_GET["titre"]));
+			}else{
+				Echo '<h1 class="text-uppercase m-4 text-center"> Aucun titre transmis </h1>';
+			}
 		?>
-		<h1 class="text-uppercase m-4 text-center">Emission</h1>	
 	<div class="margin">
 		<table class="table table-striped ">
-			<tr>
-			  <td>Nom du podcast</td>
-			  <td>Date</td>
-			  <td>Auteur</td>
-			</tr>
-			<tr>
-			  <td>Nom du podcast</td>
-			  <td>Date</td>
-			  <td>Auteur</td>
-			</tr>
-			<tr>
-			  <td>Nom du podcast</td>
-			  <td>Date</td>
-			  <td>Auteur</td>
-			</tr>
+			<?php	
+			//On analyse toutes les émissions disponibles dans un thème
+				While( $ligne = $stmt->fetch() ) {	
+					echo'<tr>
+						<td>'.$ligne["nom"].'</td>
+						<td>'.$ligne["texte"].'</td>
+						<tr>';
+					$podcast="SELECT son,image,texte
+							From Podcast
+							Where id_Emission = ?";
+					$requete = $pdo->prepare($podcast);
+					$requete->execute(array($ligne["id_emission"]));
+					//On analyse tout les podcast liées aux émissions
+					While($analyse = $requete->fetch()){
+						//On crée des liens pour aller sur la page du podcast sur laquelle on pourra écouter et télécharger celui-ci
+						echo'<td>
+								<a href="Podcast.php?son='.$analyse["son"].'&theme='.$ligne["titre"].'">'.basename($analyse["son"]).'</a>
+							</td>';
+					}
+					echo'</tr></tr>';
+				}
+			?>
 		</table>
 	</div>
 
