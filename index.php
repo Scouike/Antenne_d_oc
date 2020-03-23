da386ec6cc34ef32920d8db28a5a365295187433
Mathieu Zenone <mathieu.zenone@iut-rodez.fr>
2020-03-23 18:45:46 +0100

    integration partie 1 index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <html lang="fr">
 	<head>
 		<meta charset="utf-8">
-		<title>Acceuil</title>
+		<title>Accueil</title>
 		<!-- Lien vers boostrap -->
 		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
 		
@@ -38,115 +38,226 @@
 				include('pages/bareNav/barreNav.html'); 
 			}
 			
+			//connexion à la bd
+			$host = 'localhost';
+			$db   = 'bdradio';
+			$user = 'root';
+			$pass = 'root';
+			$charset = 'utf8mb4';
+			$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+			$options = [
+				PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES   => false,
+			];
+			try {
+				 $pdo = new PDO($dsn, $user, $pass, $options);
+			} catch (PDOException $e) {
+				 throw new PDOException($e->getMessage(), (int)$e->getCode());
+			}
+		?>
+		<p class="cadre3"> Les fréquences : Bretenoux 89.0 &nbsp; Cahors 88.1 &nbsp; Cahors sud
+						   89.0 &nbsp; Cazals 88.8 &nbsp;Figeac 88.1 &nbsp; Gourdon 105.3
+						   &nbsp; Labastide-Murat 104.1 &nbsp;  Montcuq 88.8 &nbsp; Prayssac
+						   93.7 &nbsp; Souillac 100.3 <br />
+		</p>
+		<h1 class="text-uppercase m-4 text-center">Derniers podcasts publiés</h1>
+	
+		<!-- Affichage des podcasts depuis la BD -->
+		<?php 
+			$sql = "SELECT dateCreation, podcast.id_podcast AS idpodcast, podcast.id_emission AS idemission, image, son, podcast.texte AS texte_podcast, emission.nom FROM podcast
+					JOIN emission ON podcast.id_emission = emission.id_emission
+					WHERE podcast.archive = 0
+					AND podcast.attente = 0
+					ORDER BY dateCreation DESC";
+				$stmt = $pdo->prepare($sql);
+				$stmt->execute();
+				$n = 0;
+				
+				// Boucle de 3 itérations pour afficher 3 podcasts
+				while ($n < 3 && $row = $stmt->fetch()) {
+					$n++;
+		?>		
+		<div class="mySlides transition">
+		<?php
+			affichagePodcast($row['dateCreation'], $row['idemission'], $row['idpodcast'], $row['texte_podcast'], $row['image']);
+		?>
+		</div>
+		<?php
+			}	
+		?>
+	
+		<!-- Points sous les slides -->
+		<div class="indicateurs">
+		<?php
+			for ($j = 1 ; $j <= $n ; $j++) {
+				echo "<span class='point' onclick='selectionnerSlide($j)'></span>";
+			}	
+		?>
+		</div>
+		
+		<!-- Fleches de navigation -->
+		<div>
+			<a class="suivant" onclick="changerSlide(1)">&#10095;</a>
+			<a class="precedent" onclick="changerSlide(-1)">&#10094;</a>
+		</div>	
+	
+		<?php   
+			/* inclus le code du footeur */
+			include('pages/footeur/footeurs.html'); 
 		?>
 		
-	<p class="cadre2"> Les fréquences : Bretenoux 89.0 &nbsp; Cahors 88.1 &nbsp; Cahors sud
-		89.0 &nbsp; Cazals 88.8 &nbsp;Figeac 88.1 &nbsp; Gourdon 105.3
-		&nbsp; Labastide-Murat 104.1 &nbsp;  Montcuq 88.8 &nbsp; Prayssac
-		93.7 &nbsp; Souillac 100.3 <br />
-	</p>
-	<!-- podcast avec image et texte -->
-	<div class=" cadre3 decalageGauche">
-		<div class="row">
-			<div class="col cadre_image ">
-				<img src="images/Logo.png" class="illustration" alt="Image Emission">
-			</div>
-			<div class="col ">
-				<div class="row">
-					<div class="col description"><p>Texte descriptif du podcast</p></div>
-				</div>	
-				<div class="row">
-					<div class="col"><figure>
-						<figcaption>Ecouter le podcast :</figcaption><br/>
-						<audio controls src="lien_audio">Your browser does not support the<code>audio</code> element.</audio><br/><br/>
-						<button type="button" class="btn btn-outline-success">Télécharger</button>
-						</figure>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col">LA DATE</div>
-			<div class="col">EMISSION</div>
-		</div>
-	</div>
-	<!-- /podcast avec image et texte -->
-	
-	<!-- podcast sans image avec texte -->
-	<div class=" cadre3 decalageGauche">
-		<div class="row">
-			<div class="col">
-				<div class="row">
-					<div class="col description"><p>Texte descriptif du podcast<p></div>
-				</div>
+		<?php 
+		// Fonction qui affiche un podcast
+			function affichagePodcast($date, $idemission, $podcast, $texte, $image){
+				global $pdo;
 				
-				<div class="row">
-					<div class="col"><figure>
-						<figcaption>Ecouter le podcast :</figcaption><br/>
-						<audio controls src="lien_audio">Your browser does not support the<code>audio</code> element.</audio><br/><br/>
-						<button type="button" class="btn btn-outline-success">Télécharger</button>
-						</figure>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col">LA DATE</div>
-			<div class="col">EMISSION</div>
-		</div>
-	</div>
-	<!-- /podcast sans image avec texte -->
-	
-	<!-- podcast sans image sans texte -->
-	<div class=" cadre3 decalageGauche">
-		<div class="row">
-			<div class="col">				
-				<div class="row">
-					<div class="col"><figure>
-						<figcaption>Ecouter le podcast :</figcaption><br/>
-						<audio controls src="lien_audio">Your browser does not support the<code>audio</code> element.</audio><br/><br/>
-						<button type="button" class="btn btn-outline-success">Télécharger</button>
-						</figure>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col">LA DATE</div>
-			<div class="col">EMISSION</div>
-		</div>
-	</div>
-	<!-- /podcast sans image sans texte -->
-	  
-	<!-- podcast avec image sans texte -->
-	<div class=" cadre3 decalageGauche">
-		<div class="row">
-			<div class="col cadre_image">
-				<img src="images/Logo.png" class="illustration" alt="Image Emission">
-			</div>
-			<div class="col">
-				<div class="row">
-					<div class="col"><figure>
-						<figcaption>Ecouter le podcast :</figcaption><br/>
-						<audio controls src="lien_audio">Your browser does not support the<code>audio</code> element.</audio><br/><br/>
-						<button type="button" class="btn btn-outline-success">Télécharger</button>
-						</figure>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col">LA DATE</div>
-			<div class="col">EMISSION</div>
-		</div>
-	</div>
-	  
-	
-	
-	<?php   
-			/** inclus le code du footeur */
-			include('pages/footeur/footeurs.html'); 
-	?>
+				//recuperation du nom de l'emission
+				$sql = "SELECT nom FROM emission WHERE id_emission = ? ";
+				$stmt = $pdo->prepare($sql);
+				$stmt->execute([$idemission]);	
+
+				
+				while ($row = $stmt->fetch()) {
+					$nomemission = $row['nom'];
+				}
+				
+			
+				//dertermination du type de podcast
+				if ($image != "NULL" && $texte != "NULL" ){
+					
+					//affichage d'un podcast avec une image et un texte
+					echo	'<div class=" cadre3 decalageGauche">
+							<div class="row">
+								<div class="col cadre_image ">
+									<img src="'.$image.'" class="illustration" alt="Image Emission">
+								</div>
+								<div class="col ">
+									<div class="row">
+										<div class="col description"><p>'.$texte.'</p></div>
+									</div>	
+									<div class="row">
+										<div class="col"><figure>
+											<figcaption>Ecouter le podcast :</figcaption><br/>
+											<audio controls src="'.$podcast.'">Your browser does not support the<code>audio</code> element.</audio><br/><br/>
+											<button type="button" class="btn btn-outline-success">Télécharger</button>
+											</figure>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col">date de mise en ligne : '.$date.' </div>
+								<div class="col">Emmision : '.$nomemission.'</div>
+							</div>
+						</div>';
+					
+				}else if($image == "NULL" && $texte == "NULL"){
+					//affichage d'un podcast sans image et sans texte
+					echo   '<div class=" cadre3 decalageGauche">
+								<div class="row">
+									<div class="col">				
+										<div class="row">
+											<div class="col"><figure>
+												<figcaption>Ecouter le podcast :</figcaption><br/>
+												<audio controls src="'.$podcast.'">Your browser does not support the<code>audio</code> element.</audio><br/><br/>
+												<button type="button" class="btn btn-outline-success">Télécharger</button>
+												</figure>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col">date de mise en ligne : '.$date.' </div>
+									<div class="col">Emmision : '.$nomemission.'</div>
+								</div>
+							</div>';
+					
+				}else if($image != "NULL"){
+					//affichage d'un podcast avec une image mais pas de texte
+					echo	'<div class=" cadre3 decalageGauche">
+								<div class="row">
+									<div class="col cadre_image">
+										<img src="'.$image.'" class="illustration" alt="Image Emission">
+									</div>
+									<div class="col">
+										<div class="row">
+											<div class="col"><figure>
+												<figcaption>Ecouter le podcast :</figcaption><br/>
+												<audio controls src="'.$podcast.'">Your browser does not support the<code>audio</code> element.</audio><br/><br/>
+												<button type="button" class="btn btn-outline-success">Télécharger</button>
+												</figure>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col">date de mise en ligne : '.$date.' </div>
+									<div class="col">Emmision : '.$nomemission.'</div>
+								</div>
+							</div>';
+					
+				}else{
+					//affichage d'un podcast avec un texte et pas d'image
+					echo	'<div class=" cadre3 decalageGauche">
+								<div class="row">
+									<div class="col">
+										<div class="row">
+											<div class="col description"><p>'.$texte.'<p></div>
+										</div>
+										
+										<div class="row">
+											<div class="col"><figure>
+												<figcaption>Ecouter le podcast :</figcaption><br/>
+												<audio controls src="'.$podcast.'">Your browser does not support the<code>audio</code> element.</audio><br/><br/>
+												<button type="button" class="btn btn-outline-success">Télécharger</button>
+												</figure>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col">date de mise en ligne : '.$date.' </div>
+									<div class="col">Emmision : '.$nomemission.'</div>
+								</div>
+							</div>';
+				}
+				
+			}
+		?>
+		
 	</body>
 	
 </html>
+
+<script>
+	var slideIndex = 1;
+afficherSlide(slideIndex);
+
+// boutons précédent/suivant
+function changerSlide(n) {
+  afficherSlide(slideIndex += n);
+}
+
+// Selecteur par points
+function selectionnerSlide(n) {
+  afficherSlide(slideIndex = n);
+}
+
+function afficherSlide(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var points = document.getElementsByClassName("point");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < points.length; i++) {
+      points[i].className = points[i].className.replace(" actif", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  points[slideIndex-1].className += " actif";
+  
+} 
+</script>
